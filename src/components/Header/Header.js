2308bd35e16dd212633254d51ef472bf3c8cb5d7
Benjamin Sullivan <bensully513@gmail.com>
2019-02-01 19:49:43 -0700

    changes

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../../App.css'
+
+const header = () => {
+    return (
+        <div className="hero-image">
+            <div className="hero-text">
+                <h1>Northwoods Aurora Cam</h1>
+            </div>
+      </div> 
+    )
+}
+
+export default header
